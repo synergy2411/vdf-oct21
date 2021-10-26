477a190f355fac4f5f81616d0cc5714fe24051b1
Sumit K <synergy2411@outlook.com>
2021-10-26 16:21:52 +0530

    npm commands

--- 02-node-basics/module-demo/app.js
+++ 02-node-basics/module-demo/app.js
@@ -1,4 +1,4 @@
 const {LUCKY_NUMBER, getDailyQuotes} = require('./file-module')
 
-console.log("LUCKY NUMBER : ", LUCKY_NUMBER)
+console.log("MY LUCKY NUMBER : ", LUCKY_NUMBER)
 console.log(getDailyQuotes())
