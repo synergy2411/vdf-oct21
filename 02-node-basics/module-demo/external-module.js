ffa968d29296ee7c5e706bd28757f288e560076c
Sumit K <synergy2411@outlook.com>
2021-10-26 11:50:16 +0530

    modules explained

---
+++ 02-node-basics/module-demo/external-module.js
@@ -0,0 +1,12 @@
+// const chalk = require("chalk");
+
+// console.log(chalk.green("SUCCESS Message"))
+// console.log(chalk.blue("WARNING Message"))
+// console.log(chalk.red("ERROR Message"))
+
+
+// Process Object - built-in Object
+const yargs = require("yargs")
+
+console.log("PROCESS : ", process.argv)
+console.log("YARGS : ", yargs.argv.body)
---
+++ 02-node-basics/module-demo/native-module.js
@@ -0,0 +1,67 @@
+// Native / Core Modules - path, os, file system, http, events etc
+
+// const path = require("path")
+
+// const URL = "http://www.our-domain.com/public/index.html";
+
+// console.log("Directory Name : ", path.dirname(URL));
+// console.log("File Name : ", path.basename(URL));
+// console.log("Extension Name : ", path.extname(URL));
+
+
+// const os = require("os")
+
+// console.log("Architecture : ", os.arch())
+// console.log("Total Memory : ", os.totalmem())
+// console.log("Number of CPUs : ", os.cpus().length)
+
+
+// const fs = require("fs")
+
+// console.log("START")
+// fs.readFile("./mock.txt", (err, data) => {
+//     if(err){
+//         console.log(err);
+//         process.exit(1)
+//     }
+//     // console.log(data.toString());
+//     fs.writeFile("new-file.txt", data, (err, res) => {
+//         if(err) {
+//             console.log(err);
+//             process.exit(1)
+//         }
+//         console.log(res);
+//     })
+// })
+// console.log("END");
+
+// START -> END -> Content
+
+
+
+
+// READING FILE IN SYNCHORNOUS WAY - BAD PRACTICE
+
+// console.log("START")
+// const data = fs.readFileSync("./mock.txt")
+// console.log(data)
+// console.log("END")
+
+
+
+
+
+
+
+// const http = require("http")
+
+// const server = http.createServer((request, response)=>{
+//     response.writeHead(200)
+//     response.write("The Message is SUCCESS")
+//     response.end()
+// })
+
+// // http://localhost:9090
+// server.listen(9090);
+
+// console.log("Server started at PORT : 9090")
