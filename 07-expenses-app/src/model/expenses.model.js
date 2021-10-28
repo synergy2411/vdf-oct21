d44309cfc7d02b4ffc4a04fdaed6410d4e8c1b57
Sumit K <synergy2411@outlook.com>
2021-10-28 10:31:10 +0530

    validations added

--- 07-expenses-app/src/model/expenses.model.js
+++ 07-expenses-app/src/model/expenses.model.js
@@ -5,9 +5,32 @@
 const { Schema, model } = require("mongoose");
 
 const expensesSchema = new Schema({
-    title : String,
-    amount : Number,
-    createdAt : String
+    title : {
+        type : String,
+        required : true,
+        validate : {
+            validator : (value) => {
+                return !value.includes('@')
+            },
+            message : prop => `${prop.value} should not have '@' symbol`
+        }
+    },
+    amount : {
+        type : Number,
+        required : true,
+        // min : 10,
+        max : 1000,
+        validate: {
+            validator : (value) => {
+                return value < 10
+            },
+            message : prop => `${prop.value} is lesser than 10`
+        }
+    },
+    createdAt : {
+        type : String,
+        default : new Date().toISOString()
+    }
 })
 
 const ExpensesModel = model('Expenses', expensesSchema)
